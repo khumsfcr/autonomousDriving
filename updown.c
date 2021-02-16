2b128b4f4d7980d8084821c6617a1446cd00f647
khumsfcr <cladue@khu.ac.kr>
2021-02-16 07:07:55 +0000

    Speed

---
+++ updown.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void UP(){
+
+	speed_level[0] = speed_level[0] + 1;
+	
+	return;
+}
+
+void DOWN(){
+
+	speed_level[0] = speed_level[0] - 1;
+
+	return;
+
+}
+	
