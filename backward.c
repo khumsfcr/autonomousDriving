3a27faa18805629eb7066ad7a06e3b69961ad068
khumsfcr <cladue@khu.ac.kr>
2021-02-16 07:04:31 +0000

    Dcmotor

---
+++ backward.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+int dcmotor_FR_1 = 1;
+int dcmotor_FR_2 = 4;
+int dcmotor_BR_1 = 5;
+int dcmotor_BR_2 = 6;
+int dcmotor_FL_1 = 7;
+int dcmotor_FL_2 = 0;
+int dcmotor_BL_1 = 2;
+int dcmotor_BL_2 = 3;
+int* speed_level;
+char message;
+
+void BACKWARD(int* speed_level){
+	
+	while(1){
+		softPwmWrite(dcmotor_FR_1,50*speed_level[0]);
+		softPwmWrite(dcmotor_FR_2,0);
+		softPwmWrite(dcmotor_BR_1,0);
+		softPwmWrite(dcmotor_BR_2,50*speed_level[0]);
+		softPwmWrite(dcmotor_FL_1,50*speed_level[0]);
+		softPwmWrite(dcmotor_FL_2,0);
+		softPwmWrite(dcmotor_BL_1,0);
+		softPwmWrite(dcmotor_BL_2,50*speed_level[0]);
+	}
+
+	return;
+}
+
+int main(){
+	
+	if(wiringPiSetup() == -1){
+		return 0;
+	}
+	
+	pinMode(dcmotor_FR_1,OUTPUT);
+	pinMode(dcmotor_FR_2,OUTPUT);
+	pinMode(dcmotor_BR_1,OUTPUT);
+	pinMode(dcmotor_BR_2,OUTPUT);
+	pinMode(dcmotor_FL_1,OUTPUT);
+	pinMode(dcmotor_FL_2,OUTPUT);
+	pinMode(dcmotor_BL_1,OUTPUT);
+	pinMode(dcmotor_BL_2,OUTPUT);
+
+	softPwmCreate(dcmotor_FR_1,0,200);
+	softPwmCreate(dcmotor_FR_2,0,200);
+	softPwmCreate(dcmotor_BR_1,0,200);
+	softPwmCreate(dcmotor_BR_2,0,200);
+	softPwmCreate(dcmotor_FL_1,0,200);
+	softPwmCreate(dcmotor_FL_2,0,200);
+	softPwmCreate(dcmotor_BL_1,0,200);
+	softPwmCreate(dcmotor_BL_2,0,200);
+
+	speed_level = (int*)malloc(sizeof(int)*2);
+	scanf("%c",&message);
+
+	if(message == 'B'){
+		scanf("%d",speed_level);
+		BACKWARD(speed_level);
+	}
+	return 0;
+}
+
+	
---
+++ rotate_R.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+int dcmotor_FR_1 = 1;
+int dcmotor_FR_2 = 4;
+int dcmotor_BR_1 = 5;
+int dcmotor_BR_2 = 6;
+int dcmotor_FL_1 = 7;
+int dcmotor_FL_2 = 0;
+int dcmotor_BL_1 = 2;
+int dcmotor_BL_2 = 3;
+int* speed_level;
+char message;
+
+void ROTATE_R(){
+	
+	while(1){
+		softPwmWrite(dcmotor_FR_1,200);
+		softPwmWrite(dcmotor_FR_2,0);
+		softPwmWrite(dcmotor_BR_1,0);
+		softPwmWrite(dcmotor_BR_2,200);
+		softPwmWrite(dcmotor_FL_1,0);
+		softPwmWrite(dcmotor_FL_2,200);
+		softPwmWrite(dcmotor_BL_1,200);
+		softPwmWrite(dcmotor_BL_2,0);
+	}
+
+	return;
+}
+
+int main(){
+	
+	if(wiringPiSetup() == -1){
+		return 0;
+	}
+	
+	pinMode(dcmotor_FR_1,OUTPUT);
+	pinMode(dcmotor_FR_2,OUTPUT);
+	pinMode(dcmotor_BR_1,OUTPUT);
+	pinMode(dcmotor_BR_2,OUTPUT);
+	pinMode(dcmotor_FL_1,OUTPUT);
+	pinMode(dcmotor_FL_2,OUTPUT);
+	pinMode(dcmotor_BL_1,OUTPUT);
+	pinMode(dcmotor_BL_2,OUTPUT);
+
+	softPwmCreate(dcmotor_FR_1,0,200);
+	softPwmCreate(dcmotor_FR_2,0,200);
+	softPwmCreate(dcmotor_BR_1,0,200);
+	softPwmCreate(dcmotor_BR_2,0,200);
+	softPwmCreate(dcmotor_FL_1,0,200);
+	softPwmCreate(dcmotor_FL_2,0,200);
+	softPwmCreate(dcmotor_BL_1,0,200);
+	softPwmCreate(dcmotor_BL_2,0,200);
+
+	scanf("%c",&message);
+
+	if(message == 'R'){
+		ROTATE_R();
+	}
+
+	return 0;
+}
